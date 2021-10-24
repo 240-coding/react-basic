4ef2efb5b341c266bd0408a3e7374421b3beaee4
Seoyoung Lee <wow9176@naver.com>
2021-10-24 23:56:11 +0900

    영화 앱 전체 모습 수정하기

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={poster} alt={poster} title={title} />
-      <div class="movie__data">
-        <h4 class="movie__title">{title}</h4>
-        <h5 class="movie__year">{year}</h5>
-        <p class="movie__summary">{summary}</p>
+      <div className="movie__data">
+        <h4 className="movie__title">{title}</h4>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genre">
+          {genres.map((gerne, index) => {
+            return (
+              <li key={index} className="movie__genre">
+                {gerne}
+              </li>
+            );
+          })}
+        </ul>
+        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   );
@@ -20,6 +29,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
